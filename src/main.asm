97dfa3e088376388eb5f51e60fc7dc25172b3c05
estrolabs <estrolabs@gmail.com>
2026-02-22 17:03:55 +0000

    DL: Issue with seeing output to the screen
    Although we believe it is successfully puting output to the console of firmware, we cannot see any result as right now our bootloader just exists successfully so we see no output.

--- src/main.asm
+++ src/main.asm
@@ -13,12 +13,45 @@ DEFAULT REL
 ; efi_main is the entry point of our assembly program so setting it to global here is just making sure the assembler and linker can access it.
 global efi_main
 
+; section data just means this section will go to the data part of memory where it stores data for our program.
+section .data
+
+; dw = define word (16 bit values)
+; 'H' and 'i' become 16 bit character codes
+; 13,10 = CR LF (new line in UEFI console)
+; 0 = UTF-16 terminator
+msg: dw 'H', 'i', " ", "T", "h", "e", "r", "e", 13, 10, 0
+
 ; Section text just means this section of assembly code will be loaded into the text section of memory.
 ; Code is loaded into memory and executed instructio by instruction to us its line by line but in memory its just a long list of data.
 section .text
 
 ; Entry Point efi_main is what first runs when we run our executable program.
 efi_main:
+    ; Stores the old rbx on the stack
+    push rbx
+    ; Obtain console output interface so we can call OutputString.
+    ; rdx = pointer to EFI_SYSTEM_TABLE.
+    ; +64 bytes = the ConOut field inside that struct (on x64) - basically the offset to where the ConOut is located.
+    ; tbx becomes SystemTable->ConOut - a pointer to the console output protocol.
+    mov rbx, [rdx + 64]
+
+    ; rbx currently points to the EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL object (ConOut).
+    ; The first field in that object is a function pointer called reset.
+    ; The second field is OutputString.
+    ; So OutputString is at offset +8 from the start of the protocol struct (8 byte pointers on x64)
+    mov rax, [rbx + 8]
+
+    ; arg1 = ConOut (This)
+    ; First argument = protocol pointer
+    mov rcx, rbx
+    ; arg2 = &msg (char16*)
+    ; Loafs the address of message (not the contents)
+    lea rdx, [msg]
+    ; OutputString(ConOut, msg)
+    ; Calls the function pointer
+    call rax
+
     ; rax is the 64 bit return value register in the x86_64 calling convention used by UEFI.
     ; UEFI expects your entry function to return an EFI_STATUS.
     ; EFI_SUCCESS is defined as 0
@@ -27,24 +60,13 @@ efi_main:
     ; This line of code is the standard fastest way to set a register to 0
     ; This line of code basically says use rax as the return to UEFI register and return 0 which is success.
     ; But this line does not actually return it just puts success (0) in the rax register which is used by UEFI for return values.
-    ; xor rax, rax
+    xor rax, rax
+    ; Restore the original rbx value
+    pop rbx
     ; returns from the current function to the caller.
     ; In this case the caller is UEFI firmware - specifically the UEFI loader that invoked your application entry.
     ; It uses the return address taht was pushed on the stack when UEFI called efi_main.
     ; Since rax is already 0, the firmware recieves EFI_SUCCESS
     ; using ret just says return which means it returns the value in rax to the caller which is UEFI firmware loader.
-    ; ret
-
-    ; RDX = EFI_SYSTEM_TABLE*
-    ; BootServices pointer is at offset 96 (0x60) in EFI_SYSTEM_TABLE on x64
-    mov     rbx, [rdx + 96]        ; rbx = SystemTable->BootServices
-
-    ; Stall is a function in BootServices.
-    ; In the UEFI table, Stall is after RaiseTPL and RestoreTPL:
-    ; RaiseTPL (0), RestoreTPL (8), AllocatePages (16), ... (many) ..., Stall.
-    ; To avoid guessing offsets by hand, we will NOT do Stall yet in this step.
-    ; For now, just loop forever so it's obvious you got control.
-
-    .hang:
-        jmp     .hang
+    ret
     
